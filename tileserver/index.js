e4a8b99d9abcd9b56e3004047c20e8f6418df693
James Badger <james@jamesbadger.ca>
2017-10-25 17:38:10 -0600

    Use dependent rendering
    This causes the vector tile to always be created, even if only
    raster tile clients are connecting. As both vector and raster are
    requesting the exact same data from PostGIS, there is no need to
    make two DB requests for the same data.

--- tileserver/index.js
+++ tileserver/index.js
@@ -1,29 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-var tilestrata = require('tilestrata');
-var headers = require('tilestrata-headers');
-var disk = require('tilestrata-disk');
-var mapnik = require('tilestrata-mapnik');
-var vtile = require('tilestrata-vtile');
+var express     = require('express');
+var cors        = require('cors');
+var tilestrata  = require('tilestrata');
+var headers     = require('tilestrata-headers');
+var disk        = require('tilestrata-disk');
+var vtile       = require('tilestrata-vtile');
+var vtileraster = require('tilestrata-vtile-raster');
 
-var app = express();
+var app    = express();
 var strata = tilestrata();
 
 // Define Layers
 // OSM Data on EPSG:3573
 strata.layer('3573')
-    .route('*.png')
-        .use(disk.cache({ dir: 'tilecache/3573' }))
-        .use(mapnik({
-            pathname: '/home/vagrant/carto-style/3573.xml'
-        }))
     .route('*.pbf')
         .use(headers({ 'Content-Encoding': 'gzip' }))
         .use(disk.cache({ dir: 'tilecache/3573' }))
         .use(vtile({
-            bufferSize: 512,
+            bufferSize: 128,
             xml: '/home/vagrant/carto-style/3573.xml'
-        }));
+        }))
+    .route('*.png')
+        .use(disk.cache({ dir: 'tilecache/3573' }))
+        .use(vtileraster({
+            bufferSize: 128,
+            xml: '/home/vagrant/carto-style/3573.xml'
+        }, {
+            tilesource: ['3573', '*.pbf']
+        }))
+        
+    ;
 
 app.use(cors());
 app.use(tilestrata.middleware({
